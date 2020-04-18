7d9dbcf34f227ff9dd76c4215cbe7098c7c2b61d
Grzegorz Koperwas <grzes869@gmail.com>
2020-04-18 14:42:48 +0200

    added docs

--- Assets/Scripts/LeaderBoardHandler.cs
+++ Assets/Scripts/LeaderBoardHandler.cs
@@ -1,8 +1,22 @@
-﻿using System.Collections;
+﻿/*
+LeaderBoardHandler:
+    - Co robi:
+        - Na początku rozpoczyna odliczanie czasu
+        - this.StopTime() zatrzymuje odliczanie czasu
+        - uploaduje za pomocą RestSharp czas do tablicy wyników
+        i zwraca miejsce w rankingu (UploadTime_and_get_place(username))
+    - Na czym powinien być:
+        - tak
+    - specjalne ustawienia obiektu:
+        - LevelName: nazwa poziomu
+
+*/
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Net;
 using RestSharp;
+
 public class LeaderBoardHandler : MonoBehaviour
 {
     // Start is called before the first frame update
@@ -11,18 +25,25 @@ public class LeaderBoardHandler : MonoBehaviour
     private System.DateTime startTime;
     private System.DateTime stopTime;
     private RestClient client = new RestClient("http://grzegorzkoperwas.site:5000");
+
+    // Starts timer on class load
     void Start()
     {
         startTime = System.DateTime.Now;
     }
-    // Update is called once per frame
+
+    // Stops the timer
     public void StopTime() {
         stopTime = System.DateTime.Now;
     }
+
+    // returns timer as ISO string
     public string GetFinalTimeISO() {
         var diff = stopTime.Subtract(startTime);
         return diff.ToString(@"hh\:mm\:ss");
     }
+
+    // Uploads time to server under (username) and returns place in ranking
     public int UploadTime_and_get_place(string username = "testUsername") {
         var request = new RestRequest("/api/postTime", Method.POST);
         request.AddParameter("nick", username);
@@ -43,6 +64,8 @@ public class LeaderBoardHandler : MonoBehaviour
             throw;
         }
     }
+
+    // class for RestRequest returns
     public class PostResponse {
         public bool success { get; set; }
         public int place { get; set; }
