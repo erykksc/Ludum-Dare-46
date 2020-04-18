7ee7b86b4281c501593d48ef0caaf94793a16dbc
Grzegorz Koperwas <grzes869@gmail.com>
2020-04-18 19:57:31 +0200

    SimpleEnemy done!

---
+++ Assets/Scripts/SimpleEnemy.cs
@@ -0,0 +1,52 @@
+﻿/*
+SimpleEnemy
+    - Co robi:
+        - Chodzi w lewo, uderza w coś, idzie w prawo.
+    - Na czym powinien być:
+        - na prostym przeciwniku
+    - Jakich komponentów wymaga:
+        - Rigidbody2D
+        - Collider2D ustawiony jako trigger
+    - Specjalne ustawienia:
+        - Force: wektor siły
+        - maxSpeed: prędkość maksymalna
+        - Reszta dziedziczona po Character
+
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleEnemy : Character
+{
+    [SerializeField]
+    private Vector2 Force;
+    [SerializeField]
+    private float maxSpeed;
+    private Rigidbody2D rigidbody;
+    void Start() {
+        rigidbody = GetComponent<Rigidbody2D>();    
+    }
+
+    void FixedUpdate()
+    {
+        if (rigidbody.velocity.magnitude < maxSpeed) {
+            rigidbody.AddForce(Force);
+        }
+    }
+    /// <summary>
+    /// Sent when an incoming collider makes contact with this object's
+    /// collider (2D physics only).
+    /// </summary>
+    /// <param name="other">The Collision2D data associated with this collision.</param>
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // Stop self and reverse direction
+        rigidbody.velocity = new Vector2();
+        Force.x = - Force.x;
+    }
+    void OnDrawGizmos()
+    {
+        DrawArrow.ForGizmo(transform.position, Force);
+    }
+}
