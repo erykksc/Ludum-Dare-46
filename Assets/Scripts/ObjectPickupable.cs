6dc8dfd7ffb7b6d9f0aebdbbdf9a0526a34d114c
Grzegorz Koperwas <grzes869@gmail.com>
2020-04-18 04:17:39 +0200

    naprawienie intelisense w VS code
    https://issuetracker.unity3d.com/issues/vscode-omnisharp-project-reference-warnings-are-thrown-in-vscode-console-when-using-vscode-1-dot-1-4
    
    powinno działać

---
+++ Assets/Scripts/ObjectPickupable.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPickupable : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 offset;
+    private GameObject Pickuper;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+    }
+}
