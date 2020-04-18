813b04f0feec253e58d37c3c1d5916a2f950f90c
Grzegorz Koperwas <grzes869@gmail.com>
2020-04-18 02:59:54 +0200

    my garbagio

---
+++ Assets/Scripts/myGarbagio/SlerpToLookAt.cs
@@ -0,0 +1,25 @@
+﻿ using UnityEngine;
+ 
+ public class SlerpToLookAt: MonoBehaviour
+ {
+     //values that will be set in the Inspector
+     public Transform Target;
+     public float RotationSpeed;
+ 
+     //values for internal use
+     private Quaternion _lookRotation;
+     private Vector3 _direction;
+     
+     // Update is called once per frame
+     void FixedUpdate()
+     {
+         //find the vector pointing from our position to the target
+         _direction = (Target.position - transform.position).normalized;
+ 
+         //create the rotation we need to be in to look at the target
+         _lookRotation = Quaternion.LookRotation(_direction);
+ 
+         //rotate us over time according to speed until we are in the required rotation
+         transform.rotation = Quaternion.Slerp(transform.rotation, _lookRotation, Time.fixedDeltaTime * RotationSpeed);
+     }
+ }
---
+++ Assets/Scripts/myGarbagio/armControl.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static DrawArrow;
+
+public class armControl : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float strength;
+    public Camera cam;
+    public float rest_distance;
+    public float rest_atraction;
+
+    public float maxForce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void FixedUpdate() {
+        // Add force due to mouse movment
+        var mouseSpeed3 = new Vector3(Input.GetAxis("Mouse X"), 0, Input.GetAxis("Mouse Y"));
+        if (mouseSpeed3.magnitude > maxForce) {
+            mouseSpeed3 = mouseSpeed3.normalized*maxForce;
+        }
+        rb.AddForce(mouseSpeed3*strength);
+        // Add atraction to parent
+        var parent_vector = transform.position - transform.parent.position;
+        if (parent_vector.magnitude < rest_distance) {
+            rb.AddForce(parent_vector.normalized*rest_atraction*Mathf.Pow(rest_atraction, rest_distance - parent_vector.magnitude));
+        }
+        else {
+            rb.AddForce(-parent_vector*rest_atraction*(parent_vector.magnitude - rest_distance));
+        }
+    }
+    void OnDrawGizmosSelected()
+    {
+        UnityEditor.Handles.DrawWireDisc(transform.parent.position, transform.up, rest_distance);
+    }
+}
