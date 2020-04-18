292c70c2c1cae40ff06ecb2015fb1af9c049c30b
tadeuszk <tadeuszk73+123@gmail.com>
2020-04-18 15:52:11 +0200

    Barebones Character class

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -1,50 +1,28 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Character : MonoBehaviour
+public class Character1 : MonoBehaviour
 {
-    Rigidbody2D body;
 
-    [SerializeField] float hp;
-    [SerializeField] float hpMax;
-    [SerializeField] float dmg;
-    [SerializeField] float speed;
-    [SerializeField] float jumpStrength;
-    [SerializeField] float doubleJumpStrength;
-    //distantce velocity
-    [SerializeField] float dVel;
-    [SerializeField] bool isOnGround;
-    [SerializeField] bool canDoubleJump;
-    void Start()
-    {
-        body = GetComponent<Rigidbody2D>();
-        body.freezeRotation = true;
-    }
+    //Properties
+    public int maxHP ;
+    public int HP ;
 
-    void Move(float dv)
-    {
-        if(Mathf.Abs(dv)>speed)
-        {
-            Mathf.Clamp(dv,-speed,speed);
+    private Vector3 direction;
+
+
+
+    int getHP(){ return HP; }
+
+
+    // Return true if killed
+    bool damage( int damage ){ 
+        if( (HP - damage ) <= 0 ){
+            Destroy( gameObject );
+            return true;
         }
-        body.AddForce(new Vector2(dv,0));
-    }
-    void Jump()
-    {
-        if (isOnGround )
-        {
-            body.AddForce(new Vector2(0,JumpStrength));
-        }
-        else if (canDoubleJump )
-        {
-            body.AddForce(new Vector2(0,JumpStrength));
-            canDoubleJump = false;
-        }
-    }
-    void OnCollisonEnter(Collision collision)
-    {
-        Debug.Log("Info");
-        jumpsAvailable = 2;
-    }
+
+        return false;
+
+     }
+
 }
