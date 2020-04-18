6d4860da1291a78b459b818ccbb6dd9d58edc96b
Tifix <wertynius2@gmail.com>
2020-04-18 19:00:55 +0200

    double jump
    YEEEET

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,23 +5,29 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public Rigidbody2D rb;
+    [Header("general parameters")]
     public float SpeedBezPrzedmiotu = 1.0f;
     public float SpeedZPrzedmiotem = 0.5f;
-    public float Speed;
     public float MaxSpeed = 4.0f;
     public float JumpForce = 10.0f;
     public float JumpForceWithBaby = 7.5f;
     public float DoubleJumpForce = 8.0f;
     public float DoubleJumpForceWithBaby = 5.5f;
-    public float Jump;
-    public float DoubleJump;
+    public float default_gravity = 20.0f;
     public float Masa = 0.25f;
-    public float Gravity = 20.0f;
+    public Vector2 SpawnPoint;
+
+    [Header("current parameters")]
+    public float Jump;
+    public float Speed;
+    public float DoubleJump;
+    public float Gravity; 
+
+    [Header("current state")]
     public bool Grounded;
-    public bool ReachedApex = false;
     public bool BabyInHand = false;
     public bool CanDoubleJump;
-    public Vector2 SpawnPoint;
+    
 
     // Start is called before the first frame update
     void Awake()
@@ -29,15 +35,27 @@ public class Player : MonoBehaviour
         rb = gameObject.GetComponent<Rigidbody2D>();
         rb.freezeRotation = true;
         rb.mass = Masa;
+        Gravity = default_gravity;
     }
 
-    void Start()
+    private void Update()
     {
 
+        if (Grounded && Input.GetKeyDown("w"))
+        {
+            rb.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+            CanDoubleJump = true;
+
+        }
+        else if (!Grounded && Input.GetKeyDown("w") && CanDoubleJump)
+        {
+            rb.AddForce(new Vector2(0, DoubleJumpForce), ForceMode2D.Impulse);
+            Gravity = default_gravity;
+            CanDoubleJump = false;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if (BabyInHand == true) //sprawdzanie czy ma dziecko w lapie
         {
@@ -69,21 +87,17 @@ public class Player : MonoBehaviour
             rb.AddForce(new Vector2(-Speed, 0));
         }
 
-
-
-        if (Input.GetKey("w") && Grounded == true)
+        //Gravity increase
+        if(!Grounded)      
         {
-            rb.AddForce(new Vector2(0, Jump));
+            if(Gravity<6.9f) Gravity = Gravity * 1.05f;
         }
-        if (Input.GetKey("w") && CanDoubleJump == true)
-        {
-            rb.AddForce(new Vector2(0, DoubleJump));
-            CanDoubleJump = false;
-        }
+        rb.AddForce(new Vector2(0, -Gravity));
     }
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
+        Gravity = default_gravity;
         Grounded = true;
         CanDoubleJump = false;
     }
