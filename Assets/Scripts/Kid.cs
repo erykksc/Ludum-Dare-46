52e2ab732e9b7478b8b8603b7e54313275f9b25c
andrulonis <andrulonis@spoko.pl>
2020-04-18 21:54:32 +0200

    ds

--- Assets/Scripts/Kid.cs
+++ Assets/Scripts/Kid.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 public class Kid : Character
 {
+    public Rigidbody2D rb;
+    [SerializeField] private float default_gravity = 0.4f;
+    [SerializeField] private float Gravity;
+    [SerializeField] private bool Grounded;
     [SerializeField] Vector3 offset;
     private GameObject pickUpper;
+
+    private void Awake()
+    {
+        Gravity = default_gravity;
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
     private void Start() {
         HP = 1;
     }
@@ -16,6 +27,20 @@ public class Kid : Character
         }
     }
 
+    private void FixedUpdate()
+    {
+        if (!Grounded)
+        {
+            if (Gravity < 6.9f) Gravity = Gravity * 1.05f;
+        }
+        rb.AddForce(new Vector2(0, -Gravity));
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+        {
+            Gravity = default_gravity;
+            Grounded = true;
+        }
     public void pickUp(GameObject Pickupper) {
         transform.SetParent(Pickupper.transform, false);
         pickUpper = Pickupper;
