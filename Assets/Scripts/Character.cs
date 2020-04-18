140a5723734ce30036b8f1060e52f08a68ce4ca3
tadeuszk <tadeuszk73+123@gmail.com>
2020-04-18 16:00:43 +0200

    Fixed

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -17,7 +17,7 @@ public class Character : MonoBehaviour
     // Return true if killed
     bool damage( int damage ){ 
         if( (HP - damage ) <= 0 ){
-            Destroy( gameObject );
+            Destroy( gameObject, 0.1f );
             return true;
         }
 
