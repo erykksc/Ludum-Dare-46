470f7fbb1715ca6eeb0cc133a8314625d43ceb42
Tifix <wertynius2@gmail.com>
2020-04-19 20:44:44 +0200

    Health, and damaging inheritance

--- Assets/Scripts/UI_Handler.cs
+++ Assets/Scripts/UI_Handler.cs
@@ -14,7 +14,7 @@ public class UI_Handler : MonoBehaviour
     {
         for (int i = 4; i > -1; i--)
         {
-            if (health < i+1) { Debug.Log("oof" + i); healthbar.GetChild(i).GetComponent<Image>().sprite = heart_empty;  }
+            if (health < i+1) {healthbar.GetChild(i).GetComponent<Image>().sprite = heart_empty;  }
 
         }
 
