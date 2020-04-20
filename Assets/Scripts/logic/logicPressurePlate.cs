c78537ab56aac0da554fa113b5b806232a29982e
tadueszk733 <tadeuszk73+123@gmail.com>
2020-04-20 19:54:18 +0200

    Fixed logic
    Just use me

--- Assets/Scripts/logic/logicPressurePlate.cs
+++ Assets/Scripts/logic/logicPressurePlate.cs
@@ -6,7 +6,7 @@ public class logicPressurePlate : logicInput
     //Upwenij się, że ten komponent jest jedny z logicInput na objekcie
 
     void onTouchStart(){
-        if ( inCollisionKid || inCollisionPlayer )
+        if ( inCollisionKid || inCollisionPlayer)
             state = targetState;
         updateGate();
     }
@@ -26,15 +26,5 @@ public class logicPressurePlate : logicInput
     override public void onKidTouchStart()   { onTouchStart(); }
     override public void onPlayerTouchEnd()  { onTouchEnd();   }
     override public void onKidTouchEnd()     { onTouchEnd();   }
-
-    override public bool isPlayerTryingToInteract(Collider2D collider){
-        // Check if player is trying to interact
-        string tag = collider.gameObject.tag;
-        if (tag == "Player")
-            return collider.gameObject.GetComponent<Player>().tryingToInteract;
-        if (tag == "Kid")
-            return true;
-        return false;
-    }
 
 }
