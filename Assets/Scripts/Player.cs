4d755e603793cdc12293a46d9554ec882466d9d9
tadueszk733 <tadeuszk73+123@gmail.com>
2020-04-20 12:29:36 +0200

    Added a negative

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -160,7 +160,7 @@ public class Player : Character
             }
         }
 
-        if(Input.GetKeyDown("i")&&BabyInHand()) tryingToInteract = true;
+        if(Input.GetKeyDown("i") && ! BabyInHand()) tryingToInteract = true;
         if(Input.GetKeyUp("i")) tryingToInteract = false;
         
     }
